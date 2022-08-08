3d41c8240e14d4a6f170c056517cd612877e5843
KitLuc <tobongoomez809@gmail.com>
2022-08-08 01:00:09 -0500

    Add: accent stripping

--- Encrypted/app/controller/main.js
+++ Encrypted/app/controller/main.js
@@ -9,10 +9,16 @@ const BTN_ENCRYPT = document.getElementById("btn-encrypt");
 const BTN_DECRYPT = document.getElementById("btn-decrypt");
 const BTN_COPY = document.getElementById("btn-copy");
 
+function deletAccents(TEXT) {
+    return TEXT
+        .normalize('NFD')
+        .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1")
+        .normalize();
+}
 
 BTN_ENCRYPT.onclick = () => 
 {
-    TEXT_DECRYPT.innerHTML = toEncrypted(TEXT_ENCRYPTED.value.toLowerCase());
+    TEXT_DECRYPT.innerHTML = toEncrypted(deletAccents(TEXT_ENCRYPTED.value.toLowerCase()));
     BTN_COPY.style.display = "block";
 }
 
